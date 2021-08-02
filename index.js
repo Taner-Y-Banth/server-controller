3418c8f6245067f4e519883b53c111786084ca47
Ethan Bryant <starshipinteractive@gmail.com>
2021-08-01 20:55:07 -0700

    remove hello world and yooooooooooo

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 
 const { exec } = require('child_process');
 
-a = "yoooooo"
-console.log(a)
-console.log("hello world")
 exec('cd /home/tbryant/Desktop/Minecraft/crimsonbedrock ; LD_LIBRARY_PATH=. ./bedrock_server', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
