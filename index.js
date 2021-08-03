c68542e7817edaafc3bf3bb3285a5ca99ede8c1b
Taner-Y-Banth <ethan@starshipinteractive.com>
2021-08-03 01:27:33 +0000

    adds discord integration and spawns server as child process

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
+const Discord = require("discord.js");
 
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
+const bds = spawn('bedrock_server', { cwd: '/home/tbryant/Desktop/Minecraft/crimsonbedrock', env: { LD_LIBRARY_PATH: '/home/tbryant/Desktop/Minecraft/crimsonbedrock' } });
 
-exec('cd /home/tbryant/Desktop/Minecraft/crimsonbedrock ; LD_LIBRARY_PATH=. ./bedrock_server', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
+bds.stdin.write('hello')
+
+bds.stdout.on('data', (data) => {
+    console.log(`${data}`);
 });
+
+bds.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+bds.on(' ', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+
+const prefix = '-'
+const bot = new Discord.Client();
+const token = 'Nzg5NzMwOTI1OTQzODQ4OTkx.X92UPQ.z8uU34bSWxNxz6RwNvr7r_bfumk'
+
+bot.on('ready', () => {
+    console.log('bot is ready')
+})
+
+bot.login(token)
+
+bot.on('message', async (msg) => {
+    //if our message doesnt start with our defined prefix, dont go any further into function
+    if (!msg.content.startsWith(prefix)) {
+        console.log('no prefix')
+        return
+    }
+
+    //slices off prefix from our message, then trims extra whitespace, then returns our array of words from the message
+    const args = msg.content.slice(prefix.length).trim().split(' ')
+
+    //splits off the first word from the array, which will be our command
+    const command = args.shift().toLowerCase()
+    //log the command
+    console.log('command: ', command)
+    //log any arguments passed with a command
+    console.log(args)
+
+    const message = args.join(' ');
+
+    bds.stdin.write(message)
+
+    if (command === 'command') {
+        msg.reply('One moment while I traverse the Matrix')
+    }
+})
+
