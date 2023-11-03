02293cf6a02e78278e1dbcf38233def7e6d69d17
Taner-Y-Banth <62570956+Taner-Y-Banth@users.noreply.github.com>
2023-11-02 17:59:12 -0700

    updates commands

--- index.js
+++ index.js
@@ -10,63 +10,164 @@ const endTp = 'execute in the_end run tp';
 const altaccount = '"Camera Man 3000"';
 const commandsFile = 'commands.json'; // JSON file to store custom commands
 
-let commands = [];
-
-// Load commands from JSON file
-if (fs.existsSync(commandsFile)) {
-    try {
-        commands = JSON.parse(fs.readFileSync(commandsFile));
-    } catch (error) {
-        console.error('Error loading commands from JSON file:', error);
-    }
-}
-
 const bds = spawn('./bedrock_server', {
     cwd: config.serverDirectory,
     env: { LD_LIBRARY_PATH: config.libraryPath }
 });
 
-bds.stdin.write('hello');
-
 bds.stdout.on('data', (data) => {
-    console.log(`Server Output: ${data}`);
+    console.log(`${data}`);
 });
 
 bds.stderr.on('data', (data) => {
-    console.error(`Server Error: ${data}`);
+    console.error(`stderr: ${data}`);
 });
 
-bds.on('exit', (code) => {
-    console.log(`Server process exited with code ${code}`);
+bds.on(' ', (code) => {
+    console.log(`child process exited with code ${code}`);
 });
 
 const prefix = config.prefix;
 const bot = new Discord.Client();
+const token = config.discordBotToken;
 
-bot.login(config.discordBotToken);
+bot.login(token)
+
+// Function to load custom commands from the JSON file
+function loadCommands() {
+    try {
+        const data = fs.readFileSync(commandsFile, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error loading custom commands from JSON file:', error);
+        return [];
+    }
+}
+
+let commands = loadCommands();
 
 bot.on('message', async (msg) => {
+    // If our message doesn't start with our defined prefix, don't go any further into the function
     if (!msg.content.startsWith(prefix)) {
         return;
     }
 
+    // Slices off prefix from our message, then trims extra whitespace, then returns our array of words from the message
     const args = msg.content.slice(prefix.length).trim().split(' ');
+
+    // Splits off the first word from the array, which will be our command
     const command = args.shift().toLowerCase();
 
-    if (command === 'savecommands') {
-        // Save custom commands to JSON file
-        try {
-            fs.writeFileSync(commandsFile, JSON.stringify(commands, null, 4));
-            msg.reply('Custom commands have been saved.');
-        } catch (error) {
-            console.error('Error saving custom commands to JSON file:', error);
-            msg.reply('An error occurred while saving custom commands.');
-        }
-        return;
-    }
+    const message = args.join(' ');
 
-    const foundCommand = commands.find((cmd) => cmd.name === command);
-    if (foundCommand) {
-        foundCommand.action(msg, args);
+    bds.stdin.write(message + '\n');
+
+    switch (command) {
+        case 'command':
+            msg.reply('One moment while I traverse the Matrix');
+            break;
+        case 'tpalt_o':
+            msg.reply('Your wish is my command');
+            bds.stdin.write(`${overworldTp} ${altaccount} ${message}\n`);
+            break;
+        case 'tpalt_n':
+            msg.reply('Your wish is my command');
+            bds.stdin.write(`${netherTp} ${altaccount} ${message}\n`);
+            break;
+        case 'tpalt_e':
+            msg.reply('Your wish is my command');
+            bds.stdin.write(`${endTp} ${altaccount} ${message}\n`);
+            break;
+        case 'tpalt':
+            msg.reply('Your wish is my command');
+            bds.stdin.write(`${overworldTp} ${altaccount} ${message}\n`);
+            break;
+        case 'removecommand':
+            // Implement the removecommand action here
+            const commandNameToRemove = args[0];
+            const indexToRemove = commands.findIndex((cmd) => cmd.name === commandNameToRemove);
+            if (indexToRemove !== -1) {
+                commands.splice(indexToRemove, 1);
+                message.reply(`Command '${commandNameToRemove}' removed.`);
+                fs.writeFileSync(commandsFile, JSON.stringify(commands, null, 4));
+            } else {
+                message.reply(`Command '${commandNameToRemove}' not found.`);
+            }
+            break;
+        case 'addcommand':
+        case 'addcommand_n':
+        case 'addcommand_e':
+            // Implement the addcommand, addcommand_n, and addcommand_e actions here
+            const input = args.join(' ');
+            const [newCommand, descriptionAction] = input.split('|').map((item) => item.trim());
+            if (newCommand && descriptionAction) {
+                const [newDescription, coordinates] = descriptionAction.split(' ').map((item) => item.trim());
+
+                // Check if the new command doesn't already exist
+                if (!commands.some((cmd) => cmd.name === newCommand)) {
+                    if (command === 'addcommand') {
+                        const tpDetails = `overworld, ${coordinates}`
+                    } else if (command === 'addcommand_n') {
+                        const tpDetails = `nether, ${coordinates}`
+                    } else if (command === 'addcommand_e') {
+                        const tpDetails = `the_end, ${coordinates}`
+                    }
+
+                    commands.push({
+                        name: newCommand,
+                        description: newDescription,
+                        action: tpDetails,
+                    });
+
+                    message.reply(`New command '${newCommand}' added.`);
+                    fs.writeFileSync(commandsFile, JSON.stringify(commands, null, 4));
+                } else {
+                    message.reply(`Command '${newCommand}' already exists.`);
+                }
+            } else {
+                message.reply('Invalid input format. Please use: -addcommand [name] [description] | [action]');
+            }
+            break;
+        case 'savecommands':
+            try {
+                fs.writeFileSync(commandsFile, JSON.stringify(commands, null, 4));
+                msg.reply('Custom commands have been saved.');
+            } catch (error) {
+                console.error('Error saving custom commands to JSON file:', error);
+                msg.reply('An error occurred while saving custom commands.');
+            }
+            break;
+        default:
+            // Check if it's a custom command and execute it
+            if (commands.some(cmd => cmd.name === command)) {
+                executeCustomCommand(command, msg, bds, commands);
+            } else {
+                msg.reply('Command not found.');
+            }
+            break;
     }
 });
+
+// Function to execute custom commands based on the command name
+function executeCustomCommand(commandName, msg, bds, commands) {
+    const command = commands.find(cmd => cmd.name === commandName);
+    if (command) {
+        const [dimension, coords] = command.action.split(', ').map(item => item.trim());
+        msg.reply(command.description);
+        bds.stdin.write(`${getDimensionTp(dimension)} ${altaccount} ${coords}\n`);
+    }
+}
+
+// Helper function to get the appropriate dimension TP command
+function getDimensionTp(dimension) {
+    switch (dimension) {
+        case 'overworld':
+            return overworldTp;
+        case 'nether':
+            return netherTp;
+        case 'the_end':
+            return endTp;
+        default:
+            return '';
+    }
+}
